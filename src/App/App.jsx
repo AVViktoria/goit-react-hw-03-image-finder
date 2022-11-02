ac8afb85d71630939d967eb9bd46015166a06b04
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2022-11-02 00:45:33 -0700

    did folders

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,16 +1,53 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+// import '../index.scss'
+
+
+//*      Libraries      //
+// import { nanoid } from 'nanoid';
+// import styled from 'styled-components'
+
+//*      Components      //
+// import Button from 'components/Button';
+// import ImageGallery from 'components/ImageGallery';
+// import ImageGalleryItem from 'components/ImageGalleryItem';
+// import Loader from 'components/Loader';
+// import SearchBar from 'components/SearchBar';
+
+
+
+// const API_KEY = `30114983 - 364137b9a9ec33f130a531f95`;
+// const API_LINK = `https://pixabay.com/api/?key=30114983-364137b9a9ec33f130a531f95&q=yellow+flowers&image_type=photo`;
+
+
+
+//*      Root      //
+class App extends Component {
+  state = {};
+  //
+  //*     //
+  //
+  render() {
+    //
+    return (
+      <>
+        {/* <Section >
+        <Container >
+        <h1 className = "title">Phonebook</h1>
+          <ContactForm onSubmit={this.addContact} />
+        </Container>
+        <Container >
+          <h2 className = "title">Contacts</h2>
+        <Filter value={filter}
+            onChange={this.handleChange}/>
+          <ContactList
+            contacts={visibleContacts}
+            onDeleteContactItem={this.deleteContactItem}
+          />
+          </Container>
+          </Section> */}
+      </>
+    );
+  }
+}
+
+export default App;
